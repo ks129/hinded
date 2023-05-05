483ea9792c116a927139c25c1e58279716822db1
Karlis Suvi <45097959+ks129@users.noreply.github.com>
2023-05-05 10:26:18 +0300

    Set up environment

--- hinded/settings.py
+++ hinded/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+base = environ.Path(__file__) - 2
+
+env = environ.Env(
+    DEBUG=(bool, False),
+    SECRET_KEY=(str, 'this-is-top-secret-value')
+)
+env.read_env(base('.env'))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o==zzpvxvj5)xa=kn-0+v8%16do#(ly94m45ax6-we8u9g3dij'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
