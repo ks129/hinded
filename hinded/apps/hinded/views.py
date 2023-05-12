dc0cb12813b1330521ef0c75ac585358f71aa8e9
Karlis Suvi <45097959+ks129@users.noreply.github.com>
2023-05-12 10:55:21 +0300

    Lisa koduvaade

--- hinded/apps/hinded/views.py
+++ hinded/apps/hinded/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    """Avalehe vaade."""
+
+    template_name = "hinded/home.html"
+
+    def get_context_data(self, **kwargs) -> dict:
+        """Lisa vajalikud andmed vaatele."""
+        return {"isikud": [], "hinded": []}
