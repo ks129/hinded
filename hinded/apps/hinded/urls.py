e469497a0685ced9d6eab8b51dc9530271433172
Karlis Suvi <45097959+ks129@users.noreply.github.com>
2023-05-12 10:36:29 +0300

    Fix linting

--- hinded/apps/hinded/urls.py
+++ hinded/apps/hinded/urls.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from django.views.generic import TemplateView
-
 from hinded.apps.hinded.forms import UserLoginForm
 
 urlpatterns = [
--- hinded/settings.py
+++ hinded/settings.py
@@ -74,8 +74,7 @@ ROOT_URLCONF = 'hinded.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
